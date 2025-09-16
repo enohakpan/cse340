1abb442d4c46f5056a67f48ebaae4e473b74ec19
enohakpan <dashdash59600@gmail.com>
2025-09-16 12:29:31 +0100

    Add database schema and initial SQL scripts for CSE 340 project

---
+++ database/assignment2.sql
@@ -0,0 +1,30 @@
+-- 1. Insert Tony Stark record
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- 2. Modify Tony Stark record to change account_type to "Admin"
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+
+-- 3. Delete the Tony Stark record
+DELETE FROM account
+WHERE account_email = 'tony@starkent.com';
+
+-- 4. Update GM Hummer description using REPLACE
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 5. Inner join to get Sport category inventory
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM inventory i
+INNER JOIN classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+-- 6. Update all inv_image and inv_thumbnail paths
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
+
