cc1ce5502204ead9f73d45b606edc7a5cf0fe238
enohakpan <dashdash59600@gmail.com>
2025-09-20 20:44:24 +0100

    Remove index route from server.js and clean up code

--- server.js
+++ server.js
@@ -43,8 +43,3 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
-
-// Index route  
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home"})
-})
